7ed47fb0dfa8cfa238c5d978466dcfb13bb06af6
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2025-02-23 21:42:41 +0000

    disable eslint

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ import "./src/env.js";
 const config = {
   reactStrictMode: true,
 
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
    *
