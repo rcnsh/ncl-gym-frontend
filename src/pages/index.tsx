499fa486012dc2550d5f5c0632983ce7b9443c17
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2025-03-01 03:40:41 +0000

    add types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -65,7 +65,7 @@ const chartConfig = {
 export default function Component({
   chartData,
   serverReferenceDate,
-}: InferGetServerSidePropsType) {
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const [timeRange, setTimeRange] = React.useState("30");
 
   interface ChartDataItem {
