4ba088363cdc48683e1b746160c1588a335e43f9
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2025-03-01 03:53:11 +0000

    Revert "add types"
    This reverts commit 499fa486012dc2550d5f5c0632983ce7b9443c17.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -65,7 +65,7 @@ const chartConfig = {
 export default function Component({
   chartData,
   serverReferenceDate,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetServerSidePropsType) {
   const [timeRange, setTimeRange] = React.useState("30");
 
   interface ChartDataItem {
